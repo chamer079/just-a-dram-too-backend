6a7c20058001342cd1501388013a039976bca724
chamer79 <chamer79@gmail.com>
2025-01-28 18:37:09 -0500

    installed & imported jwt & bcrypt.

--- app.py
+++ app.py
@@ -1,18 +1,35 @@
-# Import the 'Flask' class from the 'flask' library.
-from flask import Flask
+# IMPORTS
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
-# Initialize Flask
-# We'll use the pre-defined global '__name__' variable to tell Flask where it is.
+from flask import Flask, jsonify, request, g
+import jwt
+import bcrypt
+import psycopg2, psycopg2.extras
+
+
+# APP & CONFIGURATIONS / INITIALIZING FLASK
 app = Flask(__name__)
 
-# Define our route
-# This syntax is using a Python decorator, which is essentially a 
-# succinct way to wrap a function in another function.
+def get_db_connection():
+  connection = psycopg2.connect(
+    host="localhost",
+    database="whiskies_db"
+  )
+  return connection
+
+
+# AUTH ROUTES
+
+
+
+# ROUTES
 @app.route('/')
 def index():
   return "Hello, world!"
 
-# Run our application, by default on port 5000
+# SERVER HANDLER
 app.run()
 
 
