232e851f75cf5ce320dedc6c3a4255a4aa688f75
chamer79 <chamer79@gmail.com>
2025-01-29 20:27:28 -0500

    refactored & tested login route

--- app.py
+++ app.py
@@ -61,9 +61,23 @@ def sign_up():
 @app.route('/auth/login', methods=['POST'])
 def login():
   try:
-    return jsonify({"messege": "Login route reached"})
-  except Exception as err:
-    return jsonify({"err": err.message})
+    login_form_data = request.get_json()
+    connection = get_db_connection()
+    cursor = connection.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+    cursor.execute("SELECT * FROM users WHERE username = %s;", (login_form_data["username"],))
+    existing_user = cursor.fetchone()
+    if existing_user is None:
+      return jsonify({"err": "Invalid"}), 401
+    password_is_valid = bcrypt.checkpw(bytes(login_form_data["password"], 'utf-8'), bytes(existing_user["password"], 'utf-8'))
+    if not password_is_valid:
+      return jsonify({"err": "Invalid"}), 401
+    payload = {"username": existing_user["username"], "id": existing_user["id"]}
+    token = jwt.encode({"payload": payload}, os.getenv('JWT_SECRET'))
+    return jsonify({"token": token}), 200
+  except Exception as err: 
+    return jsonify({"err": err.message}), 500
+  finally:
+    connection.close
 
 
 
